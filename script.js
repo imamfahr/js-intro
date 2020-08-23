fab969b1826fa66f1cc5e401f953afed6e1c7143
imamfahr <imamfahr@gmail.com>
2020-08-23 23:06:11 +0800

    add console log data diri

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-asdasdas
+//console log data diri
+
+console.log('Imam Fahreza');
+console.log('Kalimantan Timur');
+console.log('October, 30th 19196');
+console.log('23');
+console.log('unemployment');
+
